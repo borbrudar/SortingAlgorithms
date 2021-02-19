75a7405026f264a45669bb8bd8ab41e5ad16743a
Bor Brudar <bor.brudar@gmail.com>
2021-02-19 18:45:20 +0100

    cycle code no longer retarded

--- SortingAlgorithms/Cycle.cpp
+++ SortingAlgorithms/Cycle.cpp
@@ -2,6 +2,12 @@
 
 void Cycle::sortArray(std::vector<int>& vec)
 {
+	if (isParsing) sortCycles(vec);
+	sortingSlowDown();
+}
+
+void Cycle::sortCycles(std::vector<int>& vec)
+{
 	for (int i = 0; i < vec.size(); i++) {
 		if (vec[i] == nextValue) {
 			if (nextValue == i) chooseNextCycle(vec.size());
@@ -14,30 +20,21 @@ void Cycle::sortArray(std::vector<int>& vec)
 			break;
 		}
 	}
-
-	sortingSlowDown();
 }
 
 void Cycle::chooseNextCycle(int vecSize)
 {
-	bool nextCycleChosen = false;
+	int nextCycleBegging = 0;
 
-	std::random_device rd;
-	std::default_random_engine engine(rd());
-	std::uniform_int_distribution<int> dist(0, vecSize - 1);
-	int randomNumber;
-
-	while (!nextCycleChosen) {
-		randomNumber = dist(engine);
-		nextCycleChosen = 1;
-		for (int i = 0; i < sortedValues.size(); i++) {
-			if (sortedValues[i] == randomNumber) {
-				nextCycleChosen = 0;
-				break;
-			}
+	for (int i = 0; i < sortedValues.size(); i++) {
+		if (nextCycleBegging == sortedValues[i]) {
+			nextCycleBegging++;
+			i = 0; 
+			continue;
 		}
-
 	}
 
-	nextValue = randomNumber;
+	if (nextCycleBegging == vecSize) isParsing = false;
+	else nextValue = nextCycleBegging;
+
 }
--- SortingAlgorithms/Cycle.h
+++ SortingAlgorithms/Cycle.h
@@ -7,9 +7,13 @@ public:
 	Cycle() {
 		algorithmName = "Cycle";
 	}
+
 	void sortArray(std::vector<int>& vec);
+private:
+	void sortCycles(std::vector<int>& vec);
 	void chooseNextCycle(int number);
 	
 	std::vector<int> sortedValues;
 	int nextValue = 0;
+	bool isParsing = true;
 };

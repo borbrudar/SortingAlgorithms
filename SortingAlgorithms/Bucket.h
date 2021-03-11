5615d31e5bcb4337a64fbdd4225a3a25c683ea1e
Bor Brudar <bor.brudar@gmail.com>
2021-03-11 20:53:51 +0100

    simplfying bucke and quicktree

--- SortingAlgorithms/Bucket.h
+++ SortingAlgorithms/Bucket.h
@@ -3,23 +3,20 @@
 #include <cmath>
 #include "BinaryTree.h"
 
-class Bucket : BinaryTree{
+class Bucket : public BinaryTree{
 public:
 	Bucket() = default;
 	Bucket(std::vector<int>& vec);
 	void init(std::vector<int> &vec);
 	void sortBucket();
 	std::vector<int> updateBucket();
-
-	int color = -1;
 private:
 	void updateNodes();
 	void mergeNodes();
 	void setupNewNodes();
 
 	std::vector<int> bucketArray;
-	Bucket* left = NULL;
-	Bucket* right = NULL;
+	Bucket* path[2];
 	bool isLeaf = 0, isMerging = 0;
-	int firstIterator = 0, secondIterator = 0;
+	int mainIterator[2] = { 0,0 };
 };
